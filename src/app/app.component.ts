56043e0dac091939501c7698e2966ba2b56b71b1
virendrapatidar87 <virendra.patidar87@gmail.com>
2018-10-15 00:42:43 +0530

    added css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {FormGroup, FormControl, Validators, FormsModule, } from '@angular/forms';
 import {PersonService} from './person.service';
 import { Router } from '@angular/router';
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
 
 
 @Component({
@@ -12,7 +14,13 @@ import { Router } from '@angular/router';
 export class AppComponent {
 
   
-  constructor(private router: Router) {}
+  constructor(private router: Router,private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(
+      "user",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/user.svg")
+    );
+  }
   
 
   
